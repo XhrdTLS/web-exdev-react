b3c7aaaa1079e0c03a863ab2466c7621e51c0ed6
Kmilo0816 <k4milolagos@gmail.com>
2023-07-27 20:01:10 -0400

    se añade prettier

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,12 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
