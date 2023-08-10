b319deba34be4205004e042bfc0bbae5de2e7cf5
Kmilo0816 <k4milolagos@gmail.com>
2023-08-10 12:57:21 -0400

    se agrega la barra debajo de los link en la navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,35 +2,20 @@ export const Navbar = () => {
   return (
     <nav className="sticky w-screen h-28 top-0 bg-[#F1F1F1] shadow-md flex items-center justify-between z-50 px-7">
       <a href="#" className="flex items-center h-16 gap-3">
-        <div className="flex items-center">
+        <picture className="flex items-center">
           <img src="./exdev-logo.svg" alt="logo" />
-        </div>
-        <span className="imaginotipo-text text-3xl font-bold">ExDev</span>
+        </picture>
+        <span className="imaginotipo-text text-3xl font-bold">exdev</span>
       </a>
-      <ul className="flex gap-9 justify-center items-center font-medium text-lg">
+      <ul className="flex gap-20 justify-center items-center font-medium text-lg">
         <li>
-          <a
-            href="#"
-            className="hover:text-transparent bg-clip-text bg-gradient-to-r from-[#A225C2] to-[#01B7CD] hover:border-b-2 border-rose-800"
-          >
-            Inicio
-          </a>
+          <a href="#">Inicio</a>
         </li>
         <li>
-          <a
-            href="#"
-            className="hover:text-transparent bg-clip-text bg-gradient-to-r from-[#A225C2] to-[#01B7CD] hover:border-b-2 border-rose-800 "
-          >
-            Proyectos
-          </a>
+          <a href="#">Proyectos</a>
         </li>
         <li>
-          <a
-            href="#"
-            className="hover:text-transparent bg-clip-text bg-gradient-to-r from-[#A225C2] to-[#01B7CD] hover:border-b-2 border-rose-800 "
-          >
-            Contacto
-          </a>
+          <a href="#">Contacto</a>
         </li>
       </ul>
       <div className="flex items-center">
