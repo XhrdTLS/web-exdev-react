4b861b86f2947a16429cf589f52d7feb1dbf46cd
Victor Venegas <victor.venegas.call@gmail.com>
2023-11-22 16:57:18 -0300

    Finaliznado diseño base

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const hamburgerActive = isOpen ? "fas fa-times" : "fas fa-bars";
+  const menuActive = isOpen ? "navbar__menu--active" : "";
+
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <div>
-          <img className="navbar__logo" src="./logo-exdev.svg" alt="logo" />
+          <img className="navbar__logo" src="./logo.svg" alt="logo" />
         </div>
-        <div className="navbar__menu">
-          <span className="navbar__menu--item">Inicio</span>
-          <span className="navbar__menu--item">Proyectos</span>
-          <span className="navbar__menu--item">Sobre nostros</span>
-          <button className="navbar__menu--button">Log In</button>
+        <ul className={`navbar__menu ${menuActive}`}>
+          <NavLink to="/home" className="navbar__item">
+            Inicio
+          </NavLink>
+          <NavLink to="/home" className="navbar__item">
+            Proyectos
+          </NavLink>
+          <NavLink to="/home" className="navbar__item">
+            Sobre nostros
+          </NavLink>
+        </ul>
+        <div className="navbar__toggle" onClick={handleToggle}>
+          <i className={`${hamburgerActive} navbar__hamburger`}></i>
         </div>
       </div>
     </nav>
